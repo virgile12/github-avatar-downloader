b3d770b727e639f2aa7db5cb471aa88852112781
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-11 23:26:21 +0000

    authorization

--- download_avatars.js
+++ download_avatars.js
@@ -4,10 +4,28 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
     
-    getRepoContributors("jquery", "jquery", function(err, result) {
-        console.log("Errors:", err);
-        console.log("Result:", result);
-      });
+    
 
-   
+    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+        request(url, function(err, res, body) {
+          cb(err, body);
+        });
+    var options = {
+            url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+            headers: {
+              'User-Agent': 'request',
+              'Authorization' : 'ec924a92cb68fa2291cf64f64cd5577e322bb9ef'
+          }}
+        
+          request(options, function(err, res, body) {
+            cb(err, body);
+          });
+
+
   }
+
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
