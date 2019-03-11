ff8708b140937d144be03272f900b8474822f851
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-11 23:59:06 +0000

    object loop and result avatar

--- download_avatars.js
+++ download_avatars.js
@@ -6,27 +6,32 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
     
     
-
-    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-        request(url, function(err, res, body) {
-          cb(err, body);
-        });
     var options = {
             url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
             headers: {
               'User-Agent': 'request',
-              'Authorization' : `token ${request2.GITHUB_TOKEN}`
+              'Authorization': `token ${request2.GITHUB_TOKEN}`
           }}
         
           request(options, function(err, res, body) {
             cb(err, body);
-          });
+            
+            }
+
+          );
 
 
   }
 
 
   getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+      var users = JSON.parse(result);
+      for (var user of users) {
+          console.log(user.avatar_url);
+      }
+
+      
+      
+    // console.log("Errors:", err);
+    // console.log("Result:", result);
   });
