4dacdc811d1b1946f307616cd5ca63cd106d1a72
Virgile Patrignani <virgile_patrignani@hotmail.com>
2019-03-12 00:14:42 +0000

    first img avatar test

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var request2 = require('./secret');
+var fs = require('fs')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -30,8 +31,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
           console.log(user.avatar_url);
       }
 
+    });
+    function downloadImageByURL(url, filePath) {
+        var fs = require('fs');
+
+        request.get(url)
+               .on('error', function (err) {                                  
+                 throw err; 
+               })
+               .on('response', function (response) { 
+                 console.log('downloading');
+                 response.headers['jpeg']
+        
+                 
+               })
+               .on('end', function (end) {
+                   console.log('nice job downloaded')
+               })
+               .pipe(fs.createWriteStream(filePath));
+      }
       
       
     // console.log("Errors:", err);
     // console.log("Result:", result);
-  });
+  downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
